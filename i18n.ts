36d404449f8a072b6f253043b2d31be119ca387f
Shaman <alanol@enconto.net>
2025-10-15 11:08:45 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- i18n.ts
+++ i18n.ts
@@ -2,6 +2,6 @@ import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => {
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: (await import(`./messages/${locale}.json`)).default
   };
 });
