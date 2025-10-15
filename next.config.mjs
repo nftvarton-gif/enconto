8d7be25112da7a144c7abd5de63fac1b05e2ff71
Shaman <alanol@enconto.net>
2025-10-15 13:24:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withIntl = createNextIntlPlugin('./src/i18n.ts');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default withIntl(nextConfig);
