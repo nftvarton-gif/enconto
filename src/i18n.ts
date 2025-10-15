40d3533d07a78afda4d841c79bd9eb7f5a603487
Shaman <alanol@enconto.net>
2025-10-15 12:26:34 +0000

    думаю ты что то намудрил тут

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,5 @@
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
  
-// A list of all locales that are supported
-const locales = ['en', 'ru'];
- 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
- 
-  return {
-    messages: (await import(`../messages/${locale}.json`)).default
-  };
-});
+export default getRequestConfig(async ({locale}) => ({
+  messages: (await import(`../messages/${locale}.json`)).default
+}));
