d1babd2fadbfe1599c203f85b016996a631c517c
Shaman <alanol@enconto.net>
2025-10-15 12:15:15 +0000

    исправил так что теперь весь код красный:import type { Metadata } from '

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -26,9 +26,9 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: Promise<{locale: string}>;
 }) {
-  const { locale } = params;
+  const { locale } = await params;
   const messages = await getMessages();
  
   return (
